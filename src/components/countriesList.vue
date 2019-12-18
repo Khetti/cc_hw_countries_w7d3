f2b02f910434ea24739557ed81ccb9e5b50870a2
Greg Shanks <greg.shanks@gmail.com>
2019-12-18 16:46:39 +0000

    Initial commit after clone

---
+++ src/components/countriesList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <list-country v-for="(country, index) in countries" :country="country"
+       :key="index"></list-country>
+    </ul>
+  </div>
+</template>
+<script>
+import listCountry from './listCountry.vue';
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components:{
+    "list-country": listCountry
+  }
+
+}
+</script>
+<style lang="css" scoped>
+</style>
---
+++ src/components/listCountry.vue
@@ -0,0 +1,18 @@
+<template>
+  <li v-on:click="handleClick"> {{country.name}} </li>
+</template>
+<script>
+import { eventBus } from '../main.js';
+export default {
+  name: "list-country",
+  props: ['country'],
+  methods: {
+    handleClick(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+
+}
+</script>
+<style lang="css" scoped>
+</style>
